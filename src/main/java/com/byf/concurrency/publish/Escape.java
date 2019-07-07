7626d4604ece114fe2e3c71e3b01b62f1141756d
byf312358196 <baiyifan@163.com>
2019-07-07 18:00:01 +0800

    [SafePublish] 1.get方法返回引用类型对象(防止篡改)； 2.构造未完成不要发布对象(可能过期)； Signed-off-by: byf312358196 <baiyifan@163.com>

---
+++ src/main/java/com/byf/concurrency/publish/Escape.java
@@ -0,0 +1,25 @@
+package com.byf.concurrency.publish;
+
+import com.byf.concurrency.annoations.NotRecommend;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@NotRecommend
+public class Escape {
+
+
+    public Escape(){
+        new InnerClass();
+    }
+
+    private class InnerClass{
+        public InnerClass(){
+            log.info("{}", Escape.this.thisCanBeEsape);
+        }
+    }
+    private int thisCanBeEsape = 0;
+
+    public static void main(String[] args) {
+        new Escape();
+    }
+}
